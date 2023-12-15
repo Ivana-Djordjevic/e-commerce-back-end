2889c66c51c6229741e22f8fd01d6f336ea4163d
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-12-14 22:16:26 -0800

    completed the models, associations and API routes, all functions optimally

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE' //i think
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', //dont know
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag //is this the through done properly
+  
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag // same as line 24 comment
+});
 
 module.exports = {
   Product,
